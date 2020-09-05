809fb811ba9209596f928d3c6deb1e60895a3728
张刚 <zg@bjadks.com>
2020-09-05 18:29:34 +0800

    update

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    public User GetUserById(int id) {
+        User user = userMapper.GetUserById(id);
+        return user;
+    }
+}
